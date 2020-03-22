13d7a40ffb74e2209de8b336db4a642181627766
Daniel Gomez de Souza <daniel@wyeworks.com>
2020-03-22 00:23:53 -0300

    Initialize test nonogram in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-const world = "x";
+import { Structure } from "./structure";
+import print from "./console_printer";
 
-export function hello(word: string = world): string {
-  return `Hello ${word}! `;
+const nonogram = new Structure();
+
+nonogram.width = 10;
+nonogram.height = 5;
+nonogram.rowHints = [[8], [10], [1, 8], [8], [4]];
+nonogram.colHints = [[2], [1, 1], [4], [4], [5], [5], [5], [5], [4], [3]];
+nonogram.grid = [
+  Array(10).fill(false),
+  Array(10).fill(false),
+  Array(10).fill(false),
+  Array(10).fill(false),
+  Array(10).fill(false),
+];
+
+const filled = true;
+
+if (filled) {
+  nonogram.grid[0] = [false, false, true, true, true, true, true, true, true, true];
+  nonogram.grid[1] = [true, true, true, true, true, true, true, true, true, true];
+  nonogram.grid[2] = [true, false, true, true, true, true, true, true, true, true];
+  nonogram.grid[3] = [false, true, true, true, true, true, true, true, true, false];
+  nonogram.grid[4] = [false, false, false, false, true, true, true, true, false, false];
 }
 
-console.log(hello());
+print(nonogram);
