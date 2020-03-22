b6af9dfd8ce05d9575dab2642a414137d0eef011
Daniel Gomez de Souza <daniel@wyeworks.com>
2020-03-22 00:22:53 -0300

    Write structure as a class

--- src/structure.ts
+++ src/structure.ts
@@ -1,36 +1,17 @@
-export type HintDefinition = Array<number>;
+export type Hints = Array<number>;
 
-interface Structure {
+export class Structure {
   width: number;
   height: number;
-  rowDefinitions: Array<HintDefinition>;
-  colDefinitions: Array<HintDefinition>;
+  rowHints: Array<Hints>;
+  colHints: Array<Hints>;
   grid: Array<Array<boolean>>;
-}
 
-const structure: Structure = {
-  width: 5,
-  height: 5,
-  rowDefinitions: [],
-  colDefinitions: [],
-  grid: [],
-};
-
-//https://www.nonograms.org/nonograms/i/21
-export function init(): Structure {
-  structure.width = 10;
-  structure.height = 5;
-
-  structure.rowDefinitions = [[8], [10], [1, 8], [8], [4]];
-  structure.colDefinitions = [[2], [1, 1], [4], [4], [5], [5], [5], [5], [4], [3]];
-
-  structure.grid = [
-    Array(10).fill(false),
-    Array(10).fill(false),
-    Array(10).fill(false),
-    Array(10).fill(false),
-    Array(10).fill(false),
-  ];
-
-  return structure;
+  constructor() {
+    this.width = 1;
+    this.height = 1;
+    this.rowHints = [];
+    this.colHints = [];
+    this.grid = [[false]];
+  }
 }
