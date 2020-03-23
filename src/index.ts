3891d0e1173db0976403a5caa8d3e93f26c5a464
Daniel Gomez de Souza <daniel@wyeworks.com>
2020-03-23 13:41:40 -0300

    Add more seeds

--- src/index.ts
+++ src/index.ts
@@ -54,9 +54,108 @@ function nonoThree(filled: boolean): Nonogram {
   return nonogram;
 }
 
-const nonogram = nonoThree(false);
-// nonoTwo(false);
-// nonoThree(false);
+function nonoFour(filled: boolean): Nonogram {
+  const nonogram = new Nonogram(20, 13);
+
+  nonogram.rowsRules = [
+    [3],
+    [2, 1, 1],
+    [1, 1],
+    [2, 9],
+    [1, 2, 2],
+    [1, 2, 3],
+    [4, 2, 5],
+    [1, 2, 2, 3, 2],
+    [3, 1, 1, 6, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 3, 1, 1, 3],
+    [2, 1, 2, 1],
+    [3, 3],
+  ];
+
+  nonogram.colsRules = [
+    [3],
+    [2, 1],
+    [1, 1, 3],
+    [2, 1, 1, 1],
+    [1, 2, 3, 1, 1],
+    [1, 3, 2],
+    [2, 3],
+    [3],
+    [1, 2],
+    [1, 2],
+    [1, 2],
+    [1, 2],
+    [1, 2],
+    [1, 1, 6],
+    [1, 6, 1],
+    [3, 3, 1, 3],
+    [1, 1, 1, 1],
+    [3, 1, 1],
+    [1, 2],
+    [3],
+  ];
+
+  return nonogram;
+}
+
+function nonoFive(filled: boolean): Nonogram {
+  const nonogram = new Nonogram(10, 10);
+
+  // nonogram.rowsRules = [[1, 1], [1], [1, 1]];
+  // nonogram.colsRules = [[1, 1], [1], [1, 1]];
+  nonogram.rowsRules = [
+    [2],
+    [1, 1],
+    [3, 1],
+    [2, 1],
+    [3, 1],
+    [5, 1],
+    [5],
+    [5, 1],
+    [7, 1],
+    [3, 5],
+  ];
+
+  nonogram.colsRules = [
+    [2, 1],
+    [1, 1, 1, 2],
+    [10],
+    [6],
+    [1, 5],
+    [1, 5],
+    [1, 4],
+    [1, 2],
+    [1],
+    [3],
+  ];
+
+  return nonogram;
+}
+
+function nonoSix(filled: boolean): Nonogram {
+  const nonogram = new Nonogram(7, 9);
+
+  nonogram.rowsRules = [[2, 2], [2, 2], [1, 1, 1, 1], [5], [2, 1, 2], [1, 2, 1], [2, 2], [5], [1, 1]];
+  nonogram.colsRules = [
+    [2, 3, 1],
+    [2, 2, 2],
+    [1, 2, 1, 1],
+    [3, 1],
+    [1, 2, 1],
+    [2, 2, 2],
+    [2, 3, 1],
+  ];
+
+  return nonogram;
+}
+
+// const nonogram = nonoOne(false);
+// const nonogram = nonoTwo(false);
+// const nonogram = nonoThree(false);
+// const nonogram = nonoFour(false);
+// const nonogram = nonoFive(false);
+const nonogram = nonoSix(false);
 
 print(nonogram); //empty
 
