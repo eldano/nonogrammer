0b3e7c67f9af6f5343a9341fc521d75e3e735afb
Daniel Gomez de Souza <daniel@wyeworks.com>
2020-03-24 19:50:07 -0300

    Simplify fill with 0

--- src/nonogram.ts
+++ src/nonogram.ts
@@ -15,11 +15,6 @@ export class Nonogram {
     this.colsRules = [];
     this.grid = [];
 
-    for (let row = 0; row < height; row++) {
-      this.grid[row] = [];
-      for (let col = 0; col < width; col++) {
-        this.grid[row][col] = 0;
-      }
-    }
+    this.grid = Array(height).fill(Array(width).fill(0));
   }
 }
