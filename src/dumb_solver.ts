768c936b7fc4fadec60e20e1ef76f492f950f5e9
Daniel Gomez de Souza <daniel@wyeworks.com>
2020-03-28 20:15:50 -0300

    Strategy One for dumb solver

---
+++ src/dumb_solver.ts
@@ -0,0 +1,66 @@
+import { Nonogram } from "./nonogram";
+import "./array_ext";
+
+/**
+ * Fills the squares that are definitely 1 only using the rules and the total width and height.
+ * It doesn't use the previously filled squares.
+ * @param  {Nonogram} nonogram
+ * @returns void
+ */
+function strategyOne(nonogram: Nonogram): void {
+  nonogram.grid.forEach((row, index) => {
+    const rule = nonogram.rowsRules[index];
+    const takenSpace = rule.sum() + rule.length - 1;
+    const freedom = nonogram.width - takenSpace;
+
+    let col = 0;
+    rule.forEach(ruleItem => {
+      if (ruleItem > freedom) {
+        row.splice(col, freedom, ...Array(freedom).fill(null));
+        col += freedom;
+
+        const diff = ruleItem - freedom;
+        row.splice(col, diff, ...Array(diff).fill(1));
+        col += diff;
+      } else {
+        col += ruleItem;
+      }
+
+      col += 1;
+    });
+  });
+
+  for (let index = 0; index < nonogram.width; index++) {
+    const col = nonogram.grid.map(row => row[index]);
+
+    const rule = nonogram.colsRules[index];
+    const takenSpace = rule.sum() + rule.length - 1;
+    const freedom = nonogram.height - takenSpace;
+
+    let row = 0;
+    rule.forEach(ruleItem => {
+      if (ruleItem > freedom) {
+        col.splice(row, freedom, ...Array(freedom).fill(null));
+        row += freedom;
+
+        const diff = ruleItem - freedom;
+        col.splice(row, diff, ...Array(diff).fill(1));
+        row += diff;
+      } else {
+        row += ruleItem;
+      }
+
+      row += 1;
+    });
+
+    nonogram.grid.forEach((row, colIndex) => {
+      if (row[index] === null) {
+        row[index] = col[colIndex];
+      }
+    });
+  }
+}
+
+export default function solve(nonogram: Nonogram): void {
+  strategyOne(nonogram);
+}
